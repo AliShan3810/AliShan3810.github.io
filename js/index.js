0a4416bdcb347ee2366703782460dae972092fd7
AliShan3810 <alishan3810@gmail.com>
2023-07-25 15:37:23 +0500

    Comment the portfolio for future edition

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function onDownloadResumeHandler(value){
   if(value==="1")
   {
-    window.open("docs/numan-resume.pdf")
+    window.open("docs/ali-shan-resume.pdf")
   }
   else if(value==="2")
   {
-    window.open("docs/numan-cv.pdf")
+    window.open("docs/ali-shan-cv.pdf")
   }
   document.getElementById("select-download-resume").value=0;
   
